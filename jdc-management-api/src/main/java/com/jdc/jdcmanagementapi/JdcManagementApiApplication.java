5c6217ff67cd7a782e3578c88462c31842bb09ad
OptimusZin <zin50887@gmail.com>
2024-08-26 10:12:15 +0630

    Initial Commit

---
+++ jdc-management-api/src/main/java/com/jdc/jdcmanagementapi/JdcManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.jdcmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdcManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdcManagementApiApplication.class, args);
+    }
+
+}
---
+++ jdc-management-api/src/test/java/com/jdc/jdcmanagementapi/JdcManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.jdcmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdcManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
